cb22f9c33ab9411752fc36e5d3fabd215e3cce80
Marcus Liu <liu.mingku@northeastern.edu>
2023-07-18 09:05:44 -0400

    complete the proj

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <random>
 #include <curses.h>
+
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection {
     STOP = 0,
     LEFT,
@@ -40,6 +43,16 @@ void draw() {
             } else if (i == fruitY && j == fruitX) {
                 std::cout << "F";
             } else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        std::cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print) {
+                    std::cout << "  ";
+                }
                 std::cout << "  ";
             }
         }
@@ -88,6 +101,21 @@ void input() {
 }
 
 void logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
     switch (dir) {
         case LEFT:
             x--;
@@ -109,10 +137,16 @@ void logic() {
     if (x > width || x < 0 || y > height || y < 0) {
         gameOver = true;
     }
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] ==x && tailY[i] == y) {
+            gameOver = true;
+        }
+    }
     if (x == fruitX && y == fruitY) {
         score += 10;
         fruitX  = rand() % width;
         fruitY = rand() % height;
+        nTail++;
     }
 }
 
